d1723dbc36b44f2e3fd43167e6eaf9fb3e065a76
David Hans <david.hans@front.com>
2024-11-04 10:38:47 -0300

    Release 0.0.0

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/plugin-test-sdk-install/',
   plugins: [react()],
   build: {
     rollupOptions: {
