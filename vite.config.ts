5a5bdef49616a5188e4f2edcb056a4f343f1911e
David Hans <david.hans@front.com>
2024-11-04 10:31:17 -0300

    Fix multi-page setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'multi-page/about/index.html'),
+        login: resolve(__dirname, 'multi-page/login/index.html'),
+      },
+    },
+  },
+});
